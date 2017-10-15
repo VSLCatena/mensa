86a9ad0c0f794cf145cfaa57793540d51cffb354
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-10-15 16:42:14 +0200

    Fixed: Days not showing properly

--- app/helpers.php
+++ app/helpers.php
@@ -3,5 +3,5 @@ function formatDate($date, $addEnter = false){
     $days = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
 
     $timestamp = strtotime($date);
-    return $days[date('N', $timestamp)] . ($addEnter?'<br />':' ') . date('d-m-Y H:i', $timestamp);
+    return $days[date('N', $timestamp)-1] . ($addEnter?'<br />':' ') . date('d-m-Y H:i', $timestamp);
 }
