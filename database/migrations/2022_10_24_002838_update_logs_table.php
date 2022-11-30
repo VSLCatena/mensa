7fa59fc2337b0bd4b6d3bb85150457a32eab103c
kipjr <12066560+Kipjr@users.noreply.github.com>
2022-12-01 03:50:11 +0900

    Add Visibility, severity to int, remove comments

--- database/migrations/2022_10_24_002838_update_logs_table.php
+++ database/migrations/2022_10_24_002838_update_logs_table.php
@@ -16,16 +16,9 @@ return new class extends Migration
         Schema::table('logs', function (Blueprint $table) {
             $table->dropForeign(['mensa_id']);
             $table->dropColumn('mensa_id');
-            // 0       Emergency: system is unusable
-            // 1       Alert: action must be taken immediately
-            // 2       Critical: critical conditions
-            // 3       Error: error conditions
-            // 4       Warning: warning conditions
-            // 5       Notice: normal but significant condition
-            // 6       Informational: informational messages
-            // 7       Debug: debug-level messages            
-            $table->enum('severity', [0,1,2,3,4,5,6,7])->default(6)->after('updated_at');
+            $table->int('severity')->default(5)->after('updated_at');
             $table->enum('category', ['model','mensa','mail'])->after('severity');
+            $table->int('visibility')->after('category');
             $table->nullableUuidMorphs('loggable');
 
         });
