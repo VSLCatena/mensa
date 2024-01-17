70082f9f30c8d5a37ebb0f16908ee1bebc3c6313
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-17 22:51:31 +0100

    Added provider

---
+++ api/src/providers/database.provider.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'mariadb',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      });
+    //   sequelize.addModels([Cat]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
