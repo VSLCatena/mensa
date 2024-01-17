14ba1813a874047a565863eebd4b92550b22a87f
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-17 22:54:40 +0100

    Added config for env variable usage

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseProviders } from './providers/database.provider';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
 })
