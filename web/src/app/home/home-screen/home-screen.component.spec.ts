309678e05d2765e16a4aa1ab4cd0dbe70c5214ae
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 17:43:27 +0100

    Formatting

--- web/src/app/home/home-screen/home-screen.component.spec.ts
+++ web/src/app/home/home-screen/home-screen.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeScreenComponent } from './home-screen.component';
 
 describe('HomeScreenComponent', () => {
-  let component: HomeScreenComponent;
-  let fixture: ComponentFixture<HomeScreenComponent>;
+	let component: HomeScreenComponent;
+	let fixture: ComponentFixture<HomeScreenComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomeScreenComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(HomeScreenComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [HomeScreenComponent]
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(HomeScreenComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
