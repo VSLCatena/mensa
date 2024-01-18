295b3cd37e194c1da0d22606f074c8bc919860b2
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 16:33:08 +0100

    Removed app controller and services

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { databaseProviders } from './providers/database.provider';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [ConfigModule.forRoot()],
-	controllers: [AppController],
-	providers: [AppService, ...databaseProviders]
+	providers: [...databaseProviders]
 })
 export class AppModule {}
