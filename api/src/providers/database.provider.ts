ae3a78c8955f29ea9e546d74ac5b8e9d713dec0b
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 11:00:41 +0100

    Added models

--- api/src/providers/database.provider.ts
+++ api/src/providers/database.provider.ts
@@ -1,4 +1,11 @@
 import { Sequelize } from 'sequelize-typescript';
+import { FAQ } from 'src/models/faq.model';
+import { MensaExtraOption } from 'src/models/mensa-extra-option.model';
+import { MensaUserExtraOptions } from 'src/models/mensa-user-extra-options.model';
+import { MensaUser } from 'src/models/mensa-user.model';
+import { Mensa } from 'src/models/mensa.model';
+import { MenuItem } from 'src/models/menu-item.model';
+import { User } from 'src/models/user.model';
 
 export const databaseProviders = [
   {
@@ -12,7 +19,7 @@ export const databaseProviders = [
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
       });
-    //   sequelize.addModels([Cat]);
+      sequelize.addModels([FAQ, MensaExtraOption, Mensa, MensaUser, MensaUserExtraOptions, MenuItem, User]);
       await sequelize.sync();
       return sequelize;
     },
