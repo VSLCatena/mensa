897b67d47245b6e3a28884a71157d503e5b1db03
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 22:22:29 +0100

    Added services

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,10 +5,12 @@ import { MensaController } from './controllers/mensa/mensa.controller';
 import { MensaService } from './services/mensa-service/mensa.service';
 import { MenuItemService } from './services/menu-item/menu-item.service';
 import { MensaUserService } from './services/mensa-user/mensa-user.service';
+import { MensaExtraOptionService } from './services/mensa-extra-option/mensa-extra-option.service';
+import { UserService } from './services/user/user.service';
 
 @Module({
 	imports: [ConfigModule.forRoot()],
-	providers: [...databaseProviders, MensaService, MenuItemService, MensaUserService],
+	providers: [...databaseProviders, MensaService, MenuItemService, MensaUserService, MensaExtraOptionService, UserService],
 	controllers: [MensaController]
 })
 export class AppModule {}
