46b4a4f9ae5c0fab287ca7eef19e3cc759f95554
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 21:53:05 +0100

    Added services

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { databaseProviders } from './common/providers/database.provider';
 import { ConfigModule } from '@nestjs/config';
 import { MensaController } from './controllers/mensa/mensa.controller';
-import { MensaService } from './services/mensa-service/mensa-service';
+import { MensaService } from './services/mensa-service/mensa.service';
+import { MenuItemService } from './services/menu-item/menu-item.service';
+import { MensaUserService } from './services/mensa-user/mensa-user.service';
 
 @Module({
 	imports: [ConfigModule.forRoot()],
-	providers: [...databaseProviders, MensaService],
+	providers: [...databaseProviders, MensaService, MenuItemService, MensaUserService],
 	controllers: [MensaController]
 })
 export class AppModule {}
