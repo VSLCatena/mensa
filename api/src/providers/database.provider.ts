40c12a435598da4d94dac1bed4e7849b15717d1f
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 15:06:41 +0100

    Updated order of models

--- api/src/providers/database.provider.ts
+++ api/src/providers/database.provider.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize-typescript';
-import { FAQ } from 'src/models/faq.model';
+import { Faq } from 'src/models/faq.model';
 import { MensaExtraOption } from 'src/models/mensa-extra-option.model';
 import { MensaUserExtraOption } from 'src/models/mensa-user-extra-option.model';
 import { MensaUser } from 'src/models/mensa-user.model';
@@ -19,7 +19,7 @@ export const databaseProviders = [
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
       });
-      sequelize.addModels([FAQ, MensaExtraOption, Mensa, MensaUser, MensaUserExtraOption, MenuItem, User]);
+      sequelize.addModels([ User, Mensa, Faq, MenuItem, MensaUser, MensaExtraOption, MensaUserExtraOption ]);
       await sequelize.sync();
       return sequelize;
     },
