cc8cd3aba262e496e363c7dcf4ed0f1ac229f035
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-18 17:43:09 +0100

    Added mensa model

---
+++ web/src/app/common/models/mensa.model.ts
@@ -0,0 +1,33 @@
+export class Mensa {
+	id: number;
+	title: string;
+	date: Date;
+	closingTime: Date;
+	maxUsers: number;
+	price: number;
+	closed: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+
+	constructor(
+		id: number,
+		title: string,
+		date: Date,
+		closingTime: Date,
+		maxUsers: number,
+		price: number,
+		closed: boolean,
+		createdAt: Date,
+		updatedAt: Date
+	) {
+		this.id = id;
+		this.title = title;
+		this.date = date;
+		this.closingTime = closingTime;
+		this.maxUsers = maxUsers;
+		this.price = price;
+		this.closed = closed;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+}
