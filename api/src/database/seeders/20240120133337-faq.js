c8770dcf686066b2911909c3dcb30894f41d0a47
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-20 14:37:07 +0100

    Added faqs seeder

---
+++ api/src/database/seeders/20240120133337-faq.js
@@ -0,0 +1,33 @@
+'use strict';
+import { faker } from '@faker-js/faker';
+
+const generateFaqs = () => {
+  const faqs = [];
+
+  for (let i = 1; i <= 10; i++) {
+    const faq = {
+      question: faker.lorem.sentence(),
+      answer: faker.lorem.paragraph(),
+      lastEditedBy: faker.number.int({min: 400000, max: 400010}),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    faqs.push(faq);
+  }
+
+  return faqs;
+};
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('faqs', [
+      generateFaqs(),
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('faqs', null, {});
+  }
+};
