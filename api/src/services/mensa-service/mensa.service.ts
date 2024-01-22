72bb2bff7dd5e7e17efbd2abaa3ebfca43e38d55
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-22 21:31:17 +0100

    Added first route

--- api/src/services/mensa-service/mensa.service.ts
+++ api/src/services/mensa-service/mensa.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
+import { getWeekDate } from 'src/common/helpers/date.helper';
+import { DayOfWeek } from 'src/common/types/day-of-week.type';
 import { Mensa } from 'src/database/models/mensa.model';
 
 @Injectable()
@@ -10,7 +13,16 @@ export class MensaService {
         private readonly mensaModel: typeof Mensa
     ) {}
 
-    findAll(): Promise<Mensa[]> {
-        return this.mensaModel.findAll();
+    findAll(page: number): Promise<Mensa[]> {
+        const currentWeek = getWeekDate(DayOfWeek.Monday, page);
+        const nextWeek = getWeekDate(DayOfWeek.Sunday, page);
+
+        return this.mensaModel.findAll({
+            where: {
+                date: {
+                    [Op.between]: [currentWeek, nextWeek]
+                }
+            }
+        });
     }
 }
