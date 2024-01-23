1a423892a4e0893489fe9467fec6532fef34ce6c
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-23 16:41:04 +0100

    Fixed retrieving of mensae

--- api/src/modules/mensa/service/mensa.service.ts
+++ api/src/modules/mensa/service/mensa.service.ts
@@ -1,32 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { Op } from 'sequelize';
-import { getWeekDate } from 'src/common/helpers/date.helper';
-import { MensaRepository } from 'src/common/repositories/mensa.repository';
-import { DayOfWeek } from 'src/common/types/day-of-week.type';
-import { MensaDto } from '../dto/mensa.dto';
 import { Mensa } from 'src/database/models/mensa.model';
+import { MensaRepository } from 'src/common/repositories/mensa.repository';
 import { MenuItemRepository } from 'src/common/repositories/menu-item.repository';
 import { MensaExtraOptionRepository } from 'src/common/repositories/mensa-extra-option.repository';
 import { MensaUserRepository } from 'src/common/repositories/mensa-user.repository';
+import { Op } from 'sequelize';
+import { getWeekDate } from 'src/common/helpers/date.helper';
+import { DayOfWeek } from 'src/common/types/day-of-week.type';
 import { MensaUser } from 'src/database/models/mensa-user.model';
+import { MensaDto } from '../dto/mensa.dto';
+import { User } from 'src/database/models/user.model';
 
 @Injectable()
 export class MensaService {
-
-    constructor(private readonly mensaRepository: MensaRepository, private readonly menuItemRepository: MenuItemRepository,
+    constructor(
+        private readonly mensaRepository: MensaRepository,
+        private readonly menuItemRepository: MenuItemRepository,
         private readonly mensaExtraOptionRepository: MensaExtraOptionRepository,
         private readonly mensaUserRepository: MensaUserRepository) {}
 
     async findAll(page: number): Promise<MensaDto[]> {
         const currentWeek = getWeekDate(DayOfWeek.Monday, page);
-        const nextWeek = getWeekDate(DayOfWeek.Monday, page + 1);
+        const nextWeek = getWeekDate(DayOfWeek.Monday, page + 2);
+
+        console.log('current', currentWeek);
+        console.log('next', nextWeek);
+        console.log("today", new Date())
 
         const mensae = await this.mensaRepository.findAll({
             where: {
                 date: {
                     [Op.between]: [currentWeek, nextWeek]
                 }
-            }
+            },
         });
 
         return await this.createMensaDto(mensae);
@@ -37,7 +43,7 @@ export class MensaService {
         const mensaDtoList: MensaDto[] = [];
         for (const mensa of mensae) {
             const mensaDto = new MensaDto();
-            const mensaUsers = await this.mensaUserRepository.findAll({ where: { mensaId: mensa.id } });
+            const mensaUsers = await this.mensaUserRepository.findAll({ where: { mensaId: mensa.id }, include: [User] });
             mensaDto.mensa = mensa;
             mensaDto.menuItems = await this.menuItemRepository.findAll({ where: { mensaId: mensa.id } });
             mensaDto.extraOptions = await this.mensaExtraOptionRepository.findAll({ where: { mensaId: mensa.id } });
