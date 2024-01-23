5ee21b01077688a4db3eb723ac83dfb6759881e1
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-23 21:34:01 +0100

    Added paging

--- web/src/app/home/home-screen/home-screen.component.ts
+++ web/src/app/home/home-screen/home-screen.component.ts
@@ -4,6 +4,8 @@ import { Mensa } from 'src/app/common/models/mensa.model';
 import { MenuItem } from 'src/app/common/models/menu-item.model';
 import { MensaService } from 'src/app/common/services/mensa/mensa.service';
 import { MensaDto } from 'src/app/common/models/dto/mensa.dto';
+import { TableNavigate } from 'src/app/common/types/table-navigate.type';
+import { DayOfWeek } from 'src/app/common/types/day-of-week.type';
 
 @Component({
 	selector: 'app-home-screen',
@@ -11,26 +13,27 @@ import { MensaDto } from 'src/app/common/models/dto/mensa.dto';
 	styleUrl: './home-screen.component.scss'
 })
 export class HomeScreenComponent implements OnInit {
+  private calledPages: number[] = [0];
 	public page: number = 0;
-	public mensaeDto: MensaDto[] = [];
+	public mensaeDtoList: {[page: string]: MensaDto[]} = {};
+
+  public tableNavigate = TableNavigate;
+  public dayOfWeek = DayOfWeek;
 
   constructor(private readonly router: Router, private readonly mensaService: MensaService) { }
 
   ngOnInit(): void {
     this.mensaService.getMensae().subscribe(mensaeDto => {
-      this.mensaeDto = mensaeDto;
+      this.mensaeDtoList[this.page.toString()] = mensaeDto;
     });
   }
 
-	public getDateForPage(page: number): string {
+	public getDateForPage(day: DayOfWeek, page: number): Date {
 		const today = new Date();
-		today.setDate(today.getDate() + page);
-		const options: Intl.DateTimeFormatOptions = {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		};
-		return today.toLocaleDateString('en-GB', options);
+    const distance = day - today.getDay() + 7 * page;
+    today.setDate(today.getDate() + distance);
+    today.setHours(0, 0, 0, 0);
+    return today;
 	}
 
   public getMenuItems(menuItems: MenuItem[]): MenuItem[] {
@@ -63,5 +66,19 @@ export class HomeScreenComponent implements OnInit {
 
   public shouldShowSignUpBlock(mensaDto: MensaDto): boolean {
     return mensaDto.mensa.maxUsers > mensaDto.enrollments;
+  }
+
+  public getPage(navigate: TableNavigate): void {
+    const page = navigate === TableNavigate.Previous ? this.page - 1 : this.page + 1;
+
+    if (!this.calledPages.includes(page)) {
+      this.calledPages.push(page);
+      this.mensaService.getMensae(page).subscribe(mensaeDto => {
+        this.page = page;
+        this.mensaeDtoList[this.page.toString()] = mensaeDto;
+      });
+    } else {
+      this.page = page;
+    }
   }
 }
