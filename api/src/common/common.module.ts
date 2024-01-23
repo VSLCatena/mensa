319789ed308eed12a3748b932237a943915535fe
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-23 16:11:40 +0100

    Updated export of repositories

--- api/src/common/common.module.ts
+++ api/src/common/common.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
-import { MensaExtraOptionRepository } from './repositories/mensa-extra-option.repository';
-import { MensaUserExtraOptionRepository } from './repositories/mensa-user-extra-option.repository';
-import { MensaUserRepository } from './repositories/mensa-user.repository';
-import { MenuItemRepository } from './repositories/menu-item.repository';
 import { UserRepository } from './repositories/user.repository';
 import { MensaRepository } from './repositories/mensa.repository';
+import { MenuItemRepository } from './repositories/menu-item.repository';
+import { MensaUserRepository } from './repositories/mensa-user.repository';
+import { MensaUserExtraOptionRepository } from './repositories/mensa-user-extra-option.repository';
+import { MensaExtraOptionRepository } from './repositories/mensa-extra-option.repository';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { models } from 'src/database/models.database';
+
+const repositories = [MensaExtraOptionRepository, MensaUserExtraOptionRepository, MensaUserRepository, MensaRepository, MenuItemRepository, UserRepository]
 
 @Module({
-    providers: [MensaRepository, MensaExtraOptionRepository, MensaUserExtraOptionRepository, MensaUserRepository, MenuItemRepository, UserRepository],
-    exports: [MensaRepository, MensaExtraOptionRepository, MensaUserExtraOptionRepository, MensaUserRepository, MenuItemRepository, UserRepository],
-    imports: []
+    providers: [
+        ...repositories,
+    ],
+    exports: [
+        ...repositories,
+    ],
+  imports: [SequelizeModule.forFeature(models)]
 })
 export class CommonModule { }
