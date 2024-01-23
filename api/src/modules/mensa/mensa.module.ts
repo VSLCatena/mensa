1e3c7cdeabebb8ede27c057bdd49d12622b9f3b9
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-23 16:13:39 +0100

    Added commonmodule

--- api/src/modules/mensa/mensa.module.ts
+++ api/src/modules/mensa/mensa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MensaController } from './controller/mensa.controller';
 import { MensaService } from './service/mensa.service';
+import { CommonModule } from 'src/common/common.module';
+
 @Module({
   controllers: [MensaController],
-  providers: [MensaService]
+  providers: [MensaService],
+  imports: [CommonModule]
 })
 export class MensaModule {}
