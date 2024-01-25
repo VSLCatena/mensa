7dc5e24e2c41422ff05382630bfd88fd58a64f2a
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-25 22:12:24 +0100

    Added mensa create form

--- web/src/app/mensa/mensa-create-screen/mensa-creation-screen.component.ts
+++ web/src/app/mensa/mensa-create-screen/mensa-creation-screen.component.ts
@@ -1,8 +1,65 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
 	selector: 'app-mensa-creation-screen',
 	templateUrl: './mensa-creation-screen.component.html',
 	styleUrl: './mensa-creation-screen.component.scss'
 })
-export class MensaCreationScreenComponent {}
+export class MensaCreationScreenComponent {
+	public error: Error = new Error();
+	public isLoading = false;
+	public mensaForm: FormGroup;
+
+	// Convenience getter for easy access to form controls
+	get menuControls() {
+		return (this.mensaForm.get('menu') as FormArray).controls;
+	}
+
+	get extraOptionsControls() {
+		return (this.mensaForm.get('extraOptions') as FormArray).controls;
+	}
+
+	constructor(private formBuilder: FormBuilder) {
+		this.mensaForm = this.formBuilder.group({
+			titel: new FormControl('', [Validators.required]),
+			label: new FormControl('', [Validators.required]),
+			date: new FormControl('', [Validators.required]),
+			closingTime: new FormControl('', [Validators.required]),
+			maxUsers: new FormControl('', [Validators.required]),
+			price: new FormControl(4, [Validators.required]),
+			menu: formBuilder.array([]),
+			extraOptions: formBuilder.array([]),
+		});
+
+	}
+
+	public onSubmit(): void {
+		// TODO submit form
+		console.log("SUBMIT")
+	}
+
+	public addMenuItem(): void {
+		const menuItem = this.formBuilder.group({
+			order: [(this.menuControls.length + 1), Validators.required],
+			text: ['', Validators.required],
+		});
+
+		// Add menu item to form
+		(this.mensaForm.get('menu') as FormArray).push(menuItem);
+	}
+
+	public addExtraOption(): void {
+		const extraOption = this.formBuilder.group({
+			description: ['', Validators.required],
+			price: [0, Validators.required],
+		});
+
+		// Add extra option to form
+		(this.mensaForm.get('extraOptions') as FormArray).push(extraOption);
+	}
+
+	public deleteOption(index: number, formName: string): void {
+		(this.mensaForm.get(formName) as FormArray).removeAt(index);
+	}
+}
--- web/src/app/mensa/mensa.module.ts
+++ web/src/app/mensa/mensa.module.ts
@@ -2,9 +2,11 @@ import { MensaRoutingModule } from './mensa-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MensaCreationScreenComponent } from './mensa-create-screen/mensa-creation-screen.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KeysPipe } from '../common/pipes/keys.pipe';
 
 @NgModule({
-	declarations: [MensaCreationScreenComponent],
-	imports: [MensaRoutingModule, CommonModule]
+	declarations: [MensaCreationScreenComponent, KeysPipe],
+	imports: [MensaRoutingModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class MensaModule {}
