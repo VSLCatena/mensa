f86c3b9b1484d8539a0bd54360ddf4b1e9feccfc
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-28 00:12:23 +0100

    Added create route

--- api/src/modules/mensa/controller/mensa.controller.ts
+++ api/src/modules/mensa/controller/mensa.controller.ts
@@ -1,21 +1,28 @@
-import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
-import { ApiQuery } from '@nestjs/swagger';
+import { Controller, Get, ParseIntPipe, Query, Post, Body, UsePipes, ValidationPipe, UseFilters } from '@nestjs/common';
+import { ApiBody, ApiQuery, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { MensaService } from '../service/mensa.service';
 import { MensaDto } from '../dto/mensa.dto';
+import { CreateMensaDto } from '../dto/create-mensa.dto';
 
 @Controller('mensa')
 export class MensaController {
 	constructor(private readonly mensaService: MensaService) {}
 
-	/**
-	 * @summary Gets mensae for given week + 1
-	 * @description Gets mensae for given week + 1, where the weeks starts on Monday and ends on Sunday
-	 * @param {number} page - The page number, can be negative (e.g. -1 for last week)
-	 * @returns {Mensa[]} Successful response
-	 */
 	@Get()
+	@ApiOperation({ summary: 'Gets mensae for given week + 1' })
 	@ApiQuery({ name: 'page', type: Number, required: true })
+	@ApiResponse({ status: 200, type: MensaDto, isArray: true })
 	async findAll(@Query('page', ParseIntPipe) page: number): Promise<MensaDto[]> {
 		return await this.mensaService.findAll(page);
+	}
+
+	@Post()
+	@UsePipes(new ValidationPipe({transform: true}))
+	@ApiOperation({ summary: 'Creates a new mensa' })
+	@ApiBody({ type: CreateMensaDto })
+	@ApiResponse({ status: 201, description: 'Mensa created successfully!' })
+	async create(@Body() createMensa: CreateMensaDto): Promise<{ message: string }> {
+		await this.mensaService.saveMensaDto(createMensa);
+		return { message: 'Data received successfully!' };
 	}
 }
