087839de06e5869b9dd49feee9cd0182991fe6a4
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-27 23:26:14 +0100

    Renamed menu to menuItems

--- api/src/modules/mensa/dto/create-mensa.dto.ts
+++ api/src/modules/mensa/dto/create-mensa.dto.ts
@@ -1,13 +1,43 @@
-import { MensaExtraOption } from "src/database/models/mensa-extra-option.model";
-import { MenuItem } from "src/database/models/menu-item.model";
+import { IsString, IsDate, IsInt, IsArray, ValidateNested, Matches } from 'class-validator';
+import { Type } from 'class-transformer';
+import { MensaExtraOption } from 'src/database/models/mensa-extra-option.model';
+import { MenuItem } from 'src/database/models/menu-item.model';
+import { Mensa } from 'src/database/models/mensa.model';
 
 export class CreateMensaDto {
+    @IsString()
     title: string;
-    date: string;
-    closingTime: string;
-    maxUsers: string;
-    price: string;
-    menu: MenuItem[];
-    extraOptions: MensaExtraOption[];
-}
 
+    @IsDate()
+    date: Date;
+
+    @IsDate()
+    closingTime: Date;
+
+    @IsInt()
+    maxUsers: number;
+
+    @IsString()
+	@Matches(/^\d+(\.\d{1,2})?$/)
+    price: string;
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => MenuItem)
+    menuItems: MenuItem[];
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => MensaExtraOption)
+    extraOptions: MensaExtraOption[];
+
+	public getMensa(): Mensa {
+		var mensa = new Mensa();
+		mensa.title = this.title;
+		mensa.date = this.date;
+		mensa.closingTime = this.closingTime;
+		mensa.maxUsers = this.maxUsers;
+		mensa.price = parseFloat(this.price);
+		return mensa;
+	}
+}
