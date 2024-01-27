3869520e05bf027a53b80f620b44d1cfd2edaf14
jannyboy313 <janvanoverbeek12@gmail.com>
2024-01-27 18:28:29 +0100

    Added createmensadto

--- web/src/app/common/models/dto/create-mensa.dto.ts
+++ web/src/app/common/models/dto/create-mensa.dto.ts
@@ -1,30 +1,50 @@
+import { FormArray, FormGroup } from "@angular/forms";
 import { MensaExtraOption } from "../mensa-extra-option.model";
 import { MenuItem } from "../menu-item.model";
 
 export class CreateMensaDto {
-    title: string;
-    date: string;
-    closingTime: string;
-    maxUsers: string;
-    price: string;
-    menu: MenuItem[];
-    extraOptions: MensaExtraOption[];
+    public title: string = '';
+    public date: string = '';
+    public closingTime: string = '';
+    public maxUsers: string = '';
+    public price: string = '';
+    public menu: MenuItem[] = [];
+    public extraOptions: MensaExtraOption[] = [];
 
-    constructor(
-        title: string,
-        date: string,
-        closingTime: string,
-        maxUsers: string,
-        price: string,
-        menu: MenuItem[],
-        extraOptions: MensaExtraOption[]
-    ) {
-        this.title = title;
-        this.date = date;
-        this.closingTime = closingTime;
-        this.maxUsers = maxUsers;
-        this.price = price;
-        this.menu = menu;
-        this.extraOptions = extraOptions;
+    public mapForm(form: FormGroup) {
+        this.title = form.get('title')!.value;
+        this.date = form.get('date')!.value;
+        this.closingTime = form.get('closingTime')!.value;
+        this.maxUsers = form.get('maxUsers')!.value;
+        this.price = form.get('price')!.value;
+        this.menu = this.mapMenuItems(form.get('menu')! as FormArray);
+        this.extraOptions = this.mapExtraOptions(form.get('extraOptions')! as FormArray);
     }
+
+    private mapMenuItems(formArray: FormArray): MenuItem[] {
+        let menuItems: MenuItem[] = [];
+        formArray.controls.forEach((control) => {
+            menuItems.push(new MenuItem(
+                null,
+                null,
+                control.get('order')!.value,
+                control.get('text')!.value
+            ));
+        });
+        return menuItems;
+    }
+
+    private mapExtraOptions(formArray: FormArray): MensaExtraOption[] {
+        let extraOptions: MensaExtraOption[] = [];
+        formArray.controls.forEach((control) => {
+            extraOptions.push(new MensaExtraOption(
+                null,
+                null,
+                control.get('description')!.value,
+                control.get('price')!.value
+            ));
+        });
+        return extraOptions;
+    }
+
 }
