c319b1578e070cd0a25bfb7acf6db34126186c60
jannyboy313 <janvanoverbeek12@gmail.com>
2024-02-05 07:40:24 +0100

    Fixed mensa component declaration

--- web/src/app/modules/mensa/mensa.module.ts
+++ web/src/app/modules/mensa/mensa.module.ts
@@ -6,10 +6,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { KeysPipe } from '../../common/pipes/keys.pipe';
 import { AlertComponent } from '../../common/components/alert/alert.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { MensaOverviewScreenComponent } from './mensa-overview-screen/mensa-overview-screen.component';
 import { MensaSummaryComponent } from './mensa-overview-screen/mensa-summary/mensa-summary.component';
 
 @NgModule({
-	declarations: [MensaCreationScreenComponent, KeysPipe, AlertComponent, MensaSummaryComponent],
+	declarations: [MensaCreationScreenComponent, KeysPipe, AlertComponent, MensaOverviewScreenComponent, MensaSummaryComponent],
 	imports: [
 		MensaRoutingModule,
 		CommonModule,
