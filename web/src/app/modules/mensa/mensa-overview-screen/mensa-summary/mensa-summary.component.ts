57bdcb1415c418b0c3adcd6f72a253cc4012aa74
jannyboy313 <janvanoverbeek12@gmail.com>
2024-02-05 08:33:59 +0100

    Added basic mensa summary

--- web/src/app/modules/mensa/mensa-overview-screen/mensa-summary/mensa-summary.component.ts
+++ web/src/app/modules/mensa/mensa-overview-screen/mensa-summary/mensa-summary.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Mensa } from 'src/app/common/models/mensa.model';
 
 @Component({
   selector: 'app-mensa-summary',
   templateUrl: './mensa-summary.component.html',
   styleUrl: './mensa-summary.component.scss'
 })
-export class MensaSummaryComponent {
+export class MensaSummaryComponent implements OnInit {
 
+  public mensa!: Mensa;
+  @Input({required: true}) mensaObservable!: Observable<Mensa>;
+
+  ngOnInit(): void {
+    this.mensaObservable.subscribe(mensa => {
+      this.mensa = mensa;
+    });
+  }
 }
