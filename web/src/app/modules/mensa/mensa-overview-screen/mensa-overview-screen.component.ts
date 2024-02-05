a4361a4eb8f362eb3c7dd2bce0577256adf2d693
jannyboy313 <janvanoverbeek12@gmail.com>
2024-02-05 08:34:17 +0100

    Added mensaservice

--- web/src/app/modules/mensa/mensa-overview-screen/mensa-overview-screen.component.ts
+++ web/src/app/modules/mensa/mensa-overview-screen/mensa-overview-screen.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Mensa } from 'src/app/common/models/mensa.model';
+import { MensaService } from 'src/app/common/services/mensa/mensa.service';
 
 @Component({
   selector: 'app-mensa-overview-screen',
@@ -10,11 +12,14 @@ import { Mensa } from 'src/app/common/models/mensa.model';
 export class MensaOverviewScreenComponent {
 
   public mensa!: Mensa;
+  public mensaObservable!: Observable<Mensa>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private readonly mensaService: MensaService) {
     this.route.params.subscribe( params => {
-      // Retrieve mensa
+      this.mensaObservable = this.mensaService.getMensa(params['id'])
+      this.mensaObservable.subscribe(mensa => {
+        this.mensa = mensa;
+      });
     });
-
-}
+  }
 }
