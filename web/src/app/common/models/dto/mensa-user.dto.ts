7a6acea0189ca4168c0cbff1cd04add485bba4fe
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 17:19:29 +0200

    Added mapform functionality

--- web/src/app/common/models/dto/mensa-user.dto.ts
+++ web/src/app/common/models/dto/mensa-user.dto.ts
@@ -1,31 +1,30 @@
+import { FormGroup } from "@angular/forms";
 import { IntroDto } from "./intro-dto";
 
 export class MensaUserDto {
 
-	mensaId: number;
-    isVegetarian: boolean;
-    allergies: string;
-    extraInfo: string;
-	intro: IntroDto | null;
-    volunteerDishwasher: boolean;
-    volunteerCook: boolean;
+	mensaId: number = 0;
+    isVegetarian: boolean = false;
+    allergies: string = '';
+    extraInfo: string = '';
+	intro: IntroDto | null = null;
+    volunteerDishwasher: boolean = false;
+    volunteerCook: boolean = false;
 
-    constructor(
-        mensaId: number,
-        isVegetarian: boolean,
-        allergies: string,
-        extraInfo: string,
-        intro: IntroDto | null,
-        volunteerDishwasher: boolean,
-        volunteerCook: boolean
-    ) {
-        this.mensaId = mensaId;
-        this.isVegetarian = isVegetarian;
-        this.allergies = allergies;
-        this.extraInfo = extraInfo;
-        this.intro = intro;
-        this.volunteerDishwasher = volunteerDishwasher;
-        this.volunteerCook = volunteerCook;
+    mapForm(form: FormGroup): void {
+        this.mensaId = form.get('mensaId')!.value;
+        this.isVegetarian = form.get('isVegetarian')!.value;
+        this.allergies = form.get('allergies')!.value;
+        this.extraInfo = form.get('extraInfo')!.value;
+        this.volunteerDishwasher = form.get('volunteerDishwasher')!.value;
+        this.volunteerCook = form.get('volunteerCook')!.value;
+        if (form.get('withIntro')!.value) {
+            this.intro = new IntroDto(
+                form.get('intro')!.value.isVegetarian,
+                form.get('intro')!.value.allergies,
+                form.get('intro')!.value.extraInfo
+            );
+        }
     }
 }
 
