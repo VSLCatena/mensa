6586cf85db1ef0b2d70b9ba0b201fef2dcdd0567
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 14:51:13 +0200

    Added dummy user for development

--- api/src/middlewares/user.middleware.ts
+++ api/src/middlewares/user.middleware.ts
@@ -4,11 +4,22 @@ import { Response, NextFunction } from 'express';
 import { RequestExtended } from 'src/common/contracts/request-extended.contract';
 import { User } from 'src/database/models/user.model';
 
+const defaultUser = new User();
+defaultUser.id = 1;
+defaultUser.name = 'John Deer';
+defaultUser.email = 'johndeer@hoolie.us';
+defaultUser.allergies = 'nuts, milk, gluten';
+defaultUser.vegetarian = true;
+defaultUser.mensaAdmin = true;
+defaultUser.serviceUser = false;
+defaultUser.membershipNumber = '1234567890';
+
+
 @Injectable()
 export class UserMiddleware implements NestMiddleware {
   use(req: RequestExtended, res: Response, next: NextFunction) {
     // TODO Catch the user from the database.
-    req.user = new User();
+    req.user = defaultUser;
     next();
   }
 }
