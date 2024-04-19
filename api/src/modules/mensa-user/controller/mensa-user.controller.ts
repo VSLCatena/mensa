64a41160ad5d25e38e5fc5d58e5092be004e0753
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 15:16:53 +0200

    Added feature to add users to mensa

--- api/src/modules/mensa-user/controller/mensa-user.controller.ts
+++ api/src/modules/mensa-user/controller/mensa-user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
+import { MensaUserDto } from '../dto/mensa-user.dto';
+import { MensaUserService } from '../service/mensa-user.service';
+import { User } from 'src/database/models/user.model';
+import { UserDecorator } from 'src/common/decorators/user.decorator';
 
 @Controller('mensa-user')
-export class MensaUserController {}
+export class MensaUserController {
+
+    constructor(private mensaUserService: MensaUserService) {
+
+    }
+
+    @Post()
+	@UsePipes(new ValidationPipe({ transform: true }))
+	@ApiOperation({ summary: 'Adds an user to the mensa by creating a mensa user that is connected to the mensa' })
+	@ApiBody({ type: MensaUserDto })
+	@ApiResponse({ status: 201, description: 'User added to mensa succesfully!' })
+	async create(
+		@Body() mensaUser: MensaUserDto,
+		@UserDecorator() user: User
+	): Promise<{ message: string }> {
+		await this.mensaUserService.registerUserToMensa(mensaUser, user);
+		return { message: 'Data received successfully!' };
+	}
+}
--- api/src/modules/mensa-user/service/mensa-user.service.ts
+++ api/src/modules/mensa-user/service/mensa-user.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { MensaUserDto } from '../dto/mensa-user.dto';
+import { MensaUserRepository } from 'src/common/repositories/mensa-user.repository';
+import { MensaRepository } from 'src/common/repositories/mensa.repository';
+import { User } from 'src/database/models/user.model';
 
 @Injectable()
-export class MensaUserService {}
+export class MensaUserService {
+
+	constructor(
+		private readonly mensaRepository: MensaRepository,
+		private readonly mensaUserRepository: MensaUserRepository
+	) {}
+
+    async registerUserToMensa(mensaUserDto: MensaUserDto, user: User) {
+        try {
+            this.mensaRepository.findById(mensaUserDto.mensaId); // Check if mensa exists, throws error if not
+            this.createMensaUsers(mensaUserDto, user);
+        } catch (ex) {
+            throw new NotFoundException(`Non-existing mensa for given mensa id: ${mensaUserDto.mensaId}`);
+        }
+    }
+
+    private createMensaUsers(mensaUserDto: MensaUserDto, user: User): void {
+        const mensaUser = mensaUserDto.getMensaUser(user.membershipNumber);
+        this.mensaUserRepository.create(mensaUser);
+
+        if (mensaUserDto.intro != null) {
+            const introMensaUser = mensaUserDto.getIntroMensaUser(user.membershipNumber);
+            this.mensaUserRepository.create(introMensaUser);
+        }
+    }
+}
