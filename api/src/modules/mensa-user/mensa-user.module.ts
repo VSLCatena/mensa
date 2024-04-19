5a20366fbf295c94c53e7d2332b22fe6918f1629
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 14:49:13 +0200

    Added common module for repository usage

--- api/src/modules/mensa-user/mensa-user.module.ts
+++ api/src/modules/mensa-user/mensa-user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MensaUserController } from './controller/mensa-user.controller';
 import { MensaUserService } from './service/mensa-user.service';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [MensaUserController],
-  providers: [MensaUserService]
+  providers: [MensaUserService],
+  imports: [CommonModule]
 })
 export class MensaUserModule {}
