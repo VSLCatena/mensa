842b6b2a74d7ee69a5b6fd5ef6384abf5bf2b6b2
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 15:28:44 +0200

    Fixed sequelize model initializing through build()

--- api/src/middlewares/user.middleware.ts
+++ api/src/middlewares/user.middleware.ts
@@ -4,22 +4,20 @@ import { Response, NextFunction } from 'express';
 import { RequestExtended } from 'src/common/contracts/request-extended.contract';
 import { User } from 'src/database/models/user.model';
 
-const defaultUser = new User();
-defaultUser.id = 1;
-defaultUser.name = 'John Deer';
-defaultUser.email = 'johndeer@hoolie.us';
-defaultUser.allergies = 'nuts, milk, gluten';
-defaultUser.vegetarian = true;
-defaultUser.mensaAdmin = true;
-defaultUser.serviceUser = false;
-defaultUser.membershipNumber = '1234567890';
-
-
 @Injectable()
 export class UserMiddleware implements NestMiddleware {
   use(req: RequestExtended, res: Response, next: NextFunction) {
     // TODO Catch the user from the database.
-    req.user = defaultUser;
+    req.user = User.build({
+        id: 1,
+        name: 'John Deer',
+        email: 'johndeer@hoolie.us',
+        allergies: 'nuts, milk, gluten',
+        vegetarian: true,
+        mensaAdmin: true,
+        serviceUser: false,
+        membershipNumber: '1234567890'
+    });
     next();
   }
 }
