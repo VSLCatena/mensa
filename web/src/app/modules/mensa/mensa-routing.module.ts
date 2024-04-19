a13272a7c509078937e143569ab2724cd4d184a4
Jan van Overbeek <janvanoverbeek12@gmail.com>
2024-04-19 16:29:50 +0200

    Added mensa register routing

--- web/src/app/modules/mensa/mensa-routing.module.ts
+++ web/src/app/modules/mensa/mensa-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MensaCreationScreenComponent } from './mensa-create-screen/mensa-creation-screen.component';
 import { MensaOverviewScreenComponent } from './mensa-overview-screen/mensa-overview-screen.component';
+import { MensaRegisterScreenComponent } from './mensa-register-screen/mensa-register-screen.component';
 
 const routes: Routes = [
 	{
@@ -11,7 +12,12 @@ const routes: Routes = [
 	{
 		path: ':id',
 		component: MensaOverviewScreenComponent
+	},
+	{
+		path: 'register/:id',
+		component: MensaRegisterScreenComponent
 	}
+
 ];
 
 @NgModule({
